af238ee4b1f05519da9f66babcc202dedee85044
JoelWilmot22 <818032@mystma.org>
2021-03-22 14:55:20 +0000

    Mad Lib

--- main.py
+++ main.py
@@ -8,4 +8,7 @@ noun2 = "store"
 city1 = "New York"
 pluralNoun2 = "cars"
 adjective3 = "tiny"
-print("There are many " + adjective1 + " ways to choose a "  )
+print("There are many " + adjective1 + " ways to choose a " + noun1 + " to read. First, you could ask for recommendations from your friends and " + pluralNoun1 + ". ")
+print("Just don't ask Aunt " + femaleName + " --she only reads " + adjective2 + " books with " + clothing1 + " -ripping godesses on the cover.")
+print("If your friends and family are no help, try checking out the " + noun2 + " Review in The " + city1 + " Times.")
+print("If the " + pluralNoun2 + " featured there are too " + adjective3 + " for your taste, try somthing a little more...")
